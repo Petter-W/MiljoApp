f1295f4cc6da8953c77715db21b0bd200504d45a
Petter-W <pettwr@gmail.com>
2014-04-29 14:01:28 +0200

    Fixed resteavfall.

--- scripts/categoryscript.js
+++ scripts/categoryscript.js
@@ -16,18 +16,30 @@ function getUrlVars()
 }
 
 function getCategoriesFromJSON() {
+	
 	//Get subcat from URL
 	var subcat = getUrlVars().category;
-	//Makes subcats first letter Uppercase
-	var subcatName = subcat[0].toUpperCase() + subcat.slice(1);
-	subcatName = subcatName.replace("-"," ");
-	var jsonName = "json/" + subcat + ".json";
-	$( "#mainContent" ).append("<h1>" + subcatName + "</h1>");
-	$.getJSON(jsonName).done( function( data ){
-		$.each( data.garbage, function( i, item ){
-			$( "#mainContent" ).append("<a href='item.html?subcat=" + subcat +  "&item=" + item.URLfriendlyName + "' class='linkList'>" + item.title + '</a>');
+	if(subcat === "restavfall"){
+		$( "#mainContent" ).append("<h1>Restavfall</h1>");
+		$( "#mainContent" ).append("<h3 class='tipstext'>Hva er restavfall?</h3>");
+		$( "#mainContent" ).append("<p class='tipstext'>Restavfall er det som blir igjen etter at du har sortert ut papir, plastemballasje, matavfall, glass- og metallemballasje, klær, farlig avfall, EE-avfall og grovavfall. Restavfallet kan ikke ombrukes eller gjenvinnes til nye materialer.</p>");
+		$( "#mainContent" ).append("<h3 class='tipstext'>Hvor skal dette leveres?</h3>");
+		$( "#mainContent" ).append("<p class='tipstext'>Restavfall legges i vanlig handlepose. Posen med restavfall knyttes med dobbel knute før den legges i samme avfallsbeholder som de blå og grønne posene.</p>");
+		$( "#mainContent" ).append("<h3 class='tipstext'>Hva blir det til?</h3>");
+		$( "#mainContent" ).append("<p class='tipstext'>Restavfall sendes til forbrenning med energiutnyttelse, og blir til strøm og fjernvarme.</p>");
+	}else{
+		//Makes subcats first letter Uppercase
+		var subcatName = subcat[0].toUpperCase() + subcat.slice(1);
+		subcatName = subcatName.replace("-"," ");
+		var jsonName = "json/" + subcat + ".json";
+		$( "#mainContent" ).append("<h1>" + subcatName + "</h1>");
+		$.getJSON(jsonName).done( function( data ){
+			$.each( data.garbage, function( i, item ){
+				$( "#mainContent" ).append("<a href='item.html?subcat=" + subcat +  "&item=" + item.URLfriendlyName + "' class='linkList'>" + item.title + '</a>');
+			});
 		});
-	});
+	}
+
 }
 
 
